77d61e495afc4394c85498efbeb54f56ac10684c
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-03-15 13:37:34 +0100

    Added repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,9 @@
 lock "3.8.0"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:drksnw/rails-intro.git"
+
+set :branch, "the-end"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,10 +26,19 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :deploy do
+  after :finished, :restart_puma do
+    on roles(:web) do
+      execute :sudo, 'service puma-guillaume restart'
+      execute :sudo, 'service nginx reload'
+    end
+  end
+end
